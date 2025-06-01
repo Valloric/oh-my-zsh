067d69115aff16ebec20b19639c5803c0702b49e
Val Markovic <val@markovic.io>
2025-06-01 17:15:27 +0200

    Revert "eza aliases now use pager when necessary"
    This reverts commit 138e7f11496059bd9c4d5bd4f216563052554387.

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -29,10 +29,8 @@ alias ackf="ack -a -g"
 alias h="hg"
 
 # Using eza as a fancier ls. "ll" shows hidden files as well.
-# Piping through less so that we get auto-paging when output doesn't fit on
-# screen (depends our $LESS env var config).
-alias l="eza --icons=auto --group-directories-first --color=always -l | less"
-alias ll="eza --icons=auto --group-directories-first --color=always -la | less"
+alias l="eza --icons=auto --group-directories-first -l"
+alias ll="eza --icons=auto --group-directories-first -la"
 
 # GLOBAL aliases, which work anywhere on the line. Must be surrounded by
 # whitespace and not be in quotes to be expanded.
